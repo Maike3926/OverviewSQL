df39b1eb07cdc231f506a8f027b0ebfb732443c2
Gabriel Lima <limagabriel3926u@gmail.com>
2026-05-05 20:23:29 -0300

    Create dml.sql

---
+++ Gestao_usuario/dml.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, active) VALUES 
+('João Pereira', 'joao@email.com', true),  
+('Maria Silva', 'maria@email.com', true),  
+('Ana Costa', 'ana@email.com', false), 
+('Beatriz Oliveira', 'beatriz@email.com', true), 
+('Fernanda Lima', 'fernanda@email.com', false); 
